63ce18ed28fd2dedb2b8d6672870b064b64cda81
JKharidinova <haridinova@soft-way.biz>
2024-04-17 22:49:30 +0300

    Initial

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from src import schemas, crud
@@ -8,6 +11,9 @@ from src.db import SessionLocal, engine, Base
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME,)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 
 # Dependency
@@ -38,9 +44,11 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+@app.get("/users/{user_id}", response_model=schemas.User, response_class=HTMLResponse)
+def read_user(request: Request, user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"user": db_user}
+    )
